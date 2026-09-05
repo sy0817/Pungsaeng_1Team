e0759537c77ed4a244866d6b06b56b5eebaeec53
kjw <zw7977@gmail.com>
2026-09-05 17:03:56 +0900

    백엔드 초기 세팅

---
+++ backend/src/main/java/com/likelion/pungsaeng/PungsaengApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.pungsaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PungsaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PungsaengApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/likelion/pungsaeng/PungsaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.pungsaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PungsaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
